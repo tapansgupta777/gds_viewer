5da24253fdf56baa8192d334a6ea39a4ceb8682c
Tapan <tapansgupta777@gmail.com>
2026-02-28 23:13:27 +0530

    a

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '', // this allows deployment to a subdirectory (e.g. https://tinytapeout.github.io/tinytapeout_gds_viewer/)
+  base: '/', // this allows deployment to a subdirectory (e.g. https://tinytapeout.github.io/tinytapeout_gds_viewer/)
 
   plugins: [
     {
